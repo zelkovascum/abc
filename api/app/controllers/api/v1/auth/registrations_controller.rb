136b74620183d1163e187a35833944a116bb537a
scum <kyk467575@gmail.com>
2022-04-22 19:55:16 +0900

    carrierwave動作確認

--- api/app/controllers/api/v1/auth/registrations_controller.rb
+++ api/app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
   def sign_up_params
-    params.permit(:email, :password, :password_confirmation, :name)
+    params.permit(:email, :password, :password_confirmation, :name ,:image)
   end
 end
