140af7852d4dfca75c62d726b14ec55cb1e84177
scum <kyk467575@gmail.com>
2022-05-02 03:21:31 +0900

    Header.tsx レイアウト変更

--- front/reactapp/src/components/organisms/Header.tsx
+++ front/reactapp/src/components/organisms/Header.tsx
@@ -1,6 +1,12 @@
 import { FC, memo } from "react";
 import { NavLink } from "react-router-dom";
-import { AppBar, Toolbar, IconButton, Container } from "@mui/material";
+import {
+	AppBar,
+	Toolbar,
+	IconButton,
+	Container,
+	Typography,
+} from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import AddBoxIcon from "@mui/icons-material/AddBox";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
@@ -14,53 +20,88 @@ export const Header: FC = memo(() => {
 				<Toolbar>
 					<NavLink
 						to="/"
-						style={({ isActive }) =>
-							isActive ? { color: "teal" } : { color: "inherit" }
-						}
+						style={({ isActive }) => {
+							return {
+								display: "flex",
+								alignItems: "center",
+								textDecoration: "none",
+								marginRight: "10px",
+								color: isActive ? "teal" : "inherit",
+							};
+						}}
 					>
 						<IconButton color="inherit">
 							<HomeIcon name="home" />
 						</IconButton>
+						<Typography fontSize={12}>HOME</Typography>
 					</NavLink>
 					<NavLink
 						to="/map"
-						style={({ isActive }) =>
-							isActive ? { color: "teal" } : { color: "inherit" }
-						}
+						style={({ isActive }) => {
+							return {
+								display: "flex",
+								alignItems: "center",
+								textDecoration: "none",
+								marginRight: "10px",
+								color: isActive ? "teal" : "inherit",
+							};
+						}}
 					>
 						<IconButton color="inherit">
 							<LocationOnIcon name="map" />
 						</IconButton>
+						<Typography fontSize={12}>MAP</Typography>
 					</NavLink>
 					<NavLink
 						to="/posts/new"
-						style={({ isActive }) =>
-							isActive ? { color: "teal" } : { color: "inherit" }
-						}
+						style={({ isActive }) => {
+							return {
+								display: "flex",
+								alignItems: "center",
+								textDecoration: "none",
+								marginRight: "10px",
+								color: isActive ? "teal" : "inherit",
+							};
+						}}
 					>
 						<IconButton color="inherit">
 							<AddBoxIcon name="post" />
 						</IconButton>
+						<Typography fontSize={12}>POST</Typography>
 					</NavLink>
 					<NavLink
 						to="/rooms"
-						style={({ isActive }) =>
-							isActive ? { color: "teal" } : { color: "inherit" }
-						}
+						style={({ isActive }) => {
+							return {
+								display: "flex",
+								alignItems: "center",
+								textDecoration: "none",
+								marginRight: "10px",
+								color: isActive ? "teal" : "inherit",
+							};
+						}}
 					>
 						<IconButton color="inherit">
 							<ChatIcon name="dm" />
 						</IconButton>
+						<Typography fontSize={12}>DM</Typography>
 					</NavLink>
 					<NavLink
 						to="/users/setting"
-						style={({ isActive }) =>
-							isActive ? { color: "teal" } : { color: "inherit" }
-						}
+						style={({ isActive }) => {
+							return {
+								display: "flex",
+								alignItems: "center",
+								textDecoration: "none",
+								marginRight: "10px",
+								color: isActive ? "teal" : "inherit",
+							};
+						}}
 					>
 						<IconButton color="inherit">
 							<AccountCircleIcon name="setting" />
 						</IconButton>
+						<Typography fontSize={12}>MYPAGE</Typography>
 					</NavLink>
 				</Toolbar>
 			</AppBar>
