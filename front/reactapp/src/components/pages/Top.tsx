06c1c58ee21b324fc1cbbf2ddafd00a7700ff82f
scum <kyk467575@gmail.com>
2022-05-01 16:09:07 +0900

    レイアウト変更（topページ）

--- front/reactapp/src/components/pages/Top.tsx
+++ front/reactapp/src/components/pages/Top.tsx
@@ -1,3 +1,15 @@
+import { Box, Typography } from "@mui/material";
 import { FC, memo } from "react";
 
-export const Top: FC = memo(() => <p>photudio...</p>);
+export const Top: FC = memo(() => (
+	<Box margin="auto">
+		<Box>
+			<Typography align="center">Concept</Typography>
+			<Typography align="center">Concept</Typography>
+		</Box>
+		<Box>
+			<Typography align="center">How to use</Typography>
+			<Typography align="center">How to use</Typography>
+		</Box>
+	</Box>
+));
--- front/reactapp/src/components/templates/TopLayout.tsx
+++ front/reactapp/src/components/templates/TopLayout.tsx
@@ -1,17 +1,33 @@
 import { FC, memo } from "react";
 import { Outlet } from "react-router-dom";
-import { Container, Grid } from "@mui/material";
+import { Grid, useMediaQuery } from "@mui/material";
 import { Top } from "components/pages/Top";
+import Image from "images/topImg.jpg";
+import { theme } from "providers/MuiThemeProvider";
 
-export const TopLayout: FC = memo(() => (
-	<Container maxWidth="lg">
-		<Grid container justifyContent="center">
-			<Grid item xs={6}>
+export const TopLayout: FC = memo(() => {
+	const matches = useMediaQuery(theme.breakpoints.down("md"));
+
+	return (
+		<Grid
+			container
+			justifyContent="center"
+			alignItems="center"
+			direction={matches ? "column-reverse" : undefined}
+			sx={{
+				height: matches ? "150vh" : "100vh",
+				backgroundImage: `url(${Image})`,
+				backgroundSize: "cover",
+				backgroundPosition: "center",
+				opacity: "0.8",
+			}}
+		>
+			<Grid item xs={matches ? 5 : 5}>
 				<Top />
 			</Grid>
-			<Grid item xs={6}>
+			<Grid item xs={matches ? 5 : 5}>
 				<Outlet />
 			</Grid>
 		</Grid>
-	</Container>
-));
+	);
+});
