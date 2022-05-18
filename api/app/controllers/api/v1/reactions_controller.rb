6aaada4a657c0e3de25ae130f7015cff42d6bc89
scum <kyk467575@gmail.com>
2022-05-19 01:31:11 +0900

    [fix]reaction一覧でマッチしていないユーザーのみ表示

--- api/app/controllers/api/v1/reactions_controller.rb
+++ api/app/controllers/api/v1/reactions_controller.rb
@@ -2,17 +2,9 @@ class Api::V1::ReactionsController < ApplicationController
   # from_user_id リアクションをした人
   # to_user_id   リアクションをされた人
   def index
-    received_reactions = Reaction.where(to_user_id: current_api_v1_user.id)
-    # sent_reactions = Reaction.where(from_user_id: current_api_v1_user.id)
-    # received_reactions.each do |rr|
-    #   sent_reactions.each do |sr|
-    #     if rr.from_user_id == sr.to_user_id
-
-    #     end
-    #   end
-    # end
-    reaction_users = received_reactions.map {|reaction| reaction.from_user}
-    render json: reaction_users, status: 200
+    received_reactions = Reaction.where(to_user_id: current_api_v1_user.id, matched: false)
+    reaction_users = received_reactions.map { |reaction| reaction.from_user }
+    render json: reaction_users, status: :ok
   end
 
   def create
@@ -22,34 +14,33 @@ class Api::V1::ReactionsController < ApplicationController
       from_user_id: sent_reaction.to_user_id,
       to_user_id: sent_reaction.from_user_id
     )
-    if received_reaction
-      sent_reaction.update(matched: true)
-      received_reaction.update(matched: true)
-      my_entries = Entry.where(user_id: sent_reaction.to_user_id)
-      other_entries = Entry.where(user_id: sent_reaction.from_user_id)
+    return unless received_reaction
+    sent_reaction.update(matched: true)
+    received_reaction.update(matched: true)
+    my_entries = Entry.where(user_id: sent_reaction.to_user_id)
+    other_entries = Entry.where(user_id: sent_reaction.from_user_id)
+    my_entries.each do |me|
+      other_entries.each do |oe|
+        is_room_exist = true if me.room_id == oe.room_id
+      end
+    end
+    if is_room_exist
+      # my_entries = Entry.where(user_id: sent_reaction.to_user_id)
+      # other_entries = Entry.where(user_id: sent_reaction.from_user_id)
       my_entries.each do |me|
         other_entries.each do |oe|
-          is_room_exist = true if me.room_id == oe.room_id
-        end
-      end
-      if is_room_exist
-        # my_entries = Entry.where(user_id: sent_reaction.to_user_id)
-        # other_entries = Entry.where(user_id: sent_reaction.from_user_id)
-        my_entries.each do |me|
-          other_entries.each do |oe|
-            if me.room_id == oe.room_id
-              room = Room.find_by(id: me.room_id)
-              render json: room, status: 200
-            end
+          if me.room_id == oe.room_id
+            room = Room.find_by(id: me.room_id)
+            render json: room, status: :ok
           end
         end
-      else
-        room = Room.create
-        Entry.create(room_id: room.id, user_id: sent_reaction.to_user_id)
-        Entry.create(room_id: room.id, user_id: sent_reaction.from_user_id)
-        room = Room.find_by(id: room.id)
-        render json: room, status: 200
       end
+    else
+      room = Room.create
+      Entry.create(room_id: room.id, user_id: sent_reaction.to_user_id)
+      Entry.create(room_id: room.id, user_id: sent_reaction.from_user_id)
+      room = Room.find_by(id: room.id)
+      render json: room, status: :ok
     end
   end
 
