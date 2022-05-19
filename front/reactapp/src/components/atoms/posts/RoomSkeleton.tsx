9262d14cb38747d148a70e357c7aa8aad3511afc
scum <kyk467575@gmail.com>
2022-05-20 08:03:08 +0900

    [fix]RoomSkeleton.tsx 修正

--- front/reactapp/src/components/atoms/posts/RoomSkeleton.tsx
+++ front/reactapp/src/components/atoms/posts/RoomSkeleton.tsx
@@ -4,36 +4,40 @@ import {
 	ListItemAvatar,
 	ListItemText,
 	Skeleton,
+	Divider,
 } from "@mui/material";
 
 export const RoomSkeleton: FC = memo(() => {
 	return (
-		<ListItem sx={{ mb: 1, mx: "auto" }}>
-			<ListItemAvatar>
-				<Skeleton variant="circular" width={40} height={40} />
-			</ListItemAvatar>
-			<ListItemText
-				primary={
-					<Skeleton
-						sx={{
-							width: "100px",
-						}}
-					/>
-				}
-				secondary={
-					<Skeleton
-						sx={{
-							width: {
-								xs: "200px",
-								sm: "300px",
-								md: "400px",
-								lg: "500px",
-								xl: "600px",
-							},
-						}}
-					/>
-				}
-			/>
-		</ListItem>
+		<>
+			<Divider />
+			<ListItem sx={{ mx: "auto" }}>
+				<ListItemAvatar>
+					<Skeleton variant="circular" width={40} height={40} />
+				</ListItemAvatar>
+				<ListItemText
+					primary={
+						<Skeleton
+							sx={{
+								width: "100px",
+							}}
+						/>
+					}
+					secondary={
+						<Skeleton
+							sx={{
+								width: {
+									xs: "200px",
+									sm: "300px",
+									md: "400px",
+									lg: "500px",
+									xl: "600px",
+								},
+							}}
+						/>
+					}
+				/>
+			</ListItem>
+		</>
 	);
 });
