df77ada86e1d25e447d4cd55211836609fcfc844
scum <kyk467575@gmail.com>
2022-05-22 05:13:48 +0900

    [fix]投稿コメント文字数制限変更

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :lat, :lng, :place, :date_time, :content, presence: true
-  validates :content, length: { maximum: 30 }
+  validates :content, length: { maximum: 100 }
 end
