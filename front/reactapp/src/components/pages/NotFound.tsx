364764442dfe82fa184b34a82e4c14a6bc7617cf
scum <kyk467575@gmail.com>
2022-05-21 10:45:55 +0900

    [fix]404ページレイアウト修正

--- front/reactapp/src/components/pages/NotFound.tsx
+++ front/reactapp/src/components/pages/NotFound.tsx
@@ -1,3 +1,17 @@
+import { Box, Typography } from "@mui/material";
 import { FC } from "react";
 
-export const NotFound: FC = () => <p>404エラー</p>;
+export const NotFound: FC = () => (
+	<Box
+		sx={{
+			display: "flex",
+			justifyContent: "center",
+			alignItems: "center",
+			height: "100vh",
+		}}
+	>
+		<Typography variant="h4" color="teal">
+			404 Not Found
+		</Typography>
+	</Box>
+);
