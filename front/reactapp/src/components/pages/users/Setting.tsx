dcd2c2d0fcf803a8eadedb8d9edee556616060fe
scum <kyk467575@gmail.com>
2022-05-21 13:38:33 +0900

    [fix]Setting.tsx レイアウト修正

--- front/reactapp/src/components/pages/users/Setting.tsx
+++ front/reactapp/src/components/pages/users/Setting.tsx
@@ -1,10 +1,10 @@
 import { FC, memo, useContext, useState } from "react";
-import { Link } from "react-router-dom";
-import { Avatar, Button, Box } from "@mui/material";
+import { Avatar, Button, Box, Typography, Stack, Divider } from "@mui/material";
 import { AuthContext } from "providers/AuthProvider";
 import { ImageUploadModal } from "components/molucules/users/ImageUploadModal";
 import { CurrentLocationUpdateModal } from "components/molucules/users/CurrentLocationUpdateModal";
 import { SignOutModal } from "components/molucules/users/SignOutModal";
+import { transformPlace } from "utils/transformForRead";
 
 export const Setting: FC = memo(() => {
 	const { currentUser } = useContext(AuthContext);
@@ -17,25 +17,27 @@ export const Setting: FC = memo(() => {
 
 	return (
 		<>
-			<Box sx={{ p: 1 }}>
-				<Avatar
-					alt={currentUser?.name}
-					src={`${currentUser!.image?.url}`}
-					sx={{ width: 50, height: 50 }}
-				/>
-				<Button onClick={() => setIsOpenImageUploadModal(true)}>
-					プロフィール画像を変更
-				</Button>
+			<Stack spacing={2} sx={{ p: 2, textAlign: "center" }}>
+				<Box display="flex">
+					<Avatar
+						alt={currentUser?.name}
+						src={`${currentUser!.image?.url}`}
+						sx={{ width: 50, height: 50 }}
+					/>
+					<Button onClick={() => setIsOpenImageUploadModal(true)}>
+						プロフィール画像を変更
+					</Button>
+				</Box>
+				<Divider />
+				<Typography>ユーザーネーム: {currentUser?.name}</Typography>
+				<Divider />
+				<Typography>住所: {transformPlace(currentUser?.address!)}</Typography>
 				<Button onClick={() => setIsOpenCurrentLocationUpdateModal(true)}>
 					住所を変更
 				</Button>
-				<p>ユーザーネーム: {currentUser?.name}</p>
-				<p>住所: {currentUser?.address}</p>
-				<Button>
-					<Link to="/users/reactions">reactions</Link>
-				</Button>
+				<Divider />
 				<Button onClick={() => setIsOpenSignOutModal(true)}>ログアウト</Button>
-			</Box>
+			</Stack>
 			<SignOutModal
 				isOpenModal={isOpenSignOutModal}
 				setIsOpenModal={setIsOpenSignOutModal}
