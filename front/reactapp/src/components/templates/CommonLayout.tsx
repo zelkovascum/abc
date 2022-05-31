a49ec58da9fc04bde556b07f6682f7818041385b
scum <kyk467575@gmail.com>
2022-06-01 01:41:31 +0900

    [clean]リファクタリング

--- front/reactapp/src/components/templates/CommonLayout.tsx
+++ front/reactapp/src/components/templates/CommonLayout.tsx
@@ -1,28 +1,18 @@
-import { FC, memo, useContext } from "react";
+import { FC, memo } from "react";
 import { Outlet } from "react-router-dom";
 import { Box, Grid } from "@mui/material";
 import { Header } from "components/organisms/Header";
 import { Footer } from "components/organisms/Footer";
-import { ReactionAlert } from "components/molucules/reactions/ReactionAlert";
-import { ReactionNotificationContext } from "providers/ReactionNotificationProvider";
+import { Alerts } from "components/pages/Alerts";
 
 export const CommonLayout: FC = memo(() => {
-	const { notificationState, notificationDispatch } = useContext(
-		ReactionNotificationContext
-	);
-
 	return (
 		<Box>
 			<Header />
 			<Grid container justifyContent="center" py={"64px"}>
 				<Grid item>
 					<Outlet />
-					<ReactionAlert
-						open={notificationState.isAlertMessageOpen}
-						dispatch={notificationDispatch}
-						severity="info"
-						message={notificationState.message}
-					/>
+					<Alerts />
 				</Grid>
 			</Grid>
 			<Footer />
