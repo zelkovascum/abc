998286ec46e0d7f4cc2e9c802c15fd69eb771cfd
scum <kyk467575@gmail.com>
2022-06-02 02:51:37 +0900

    [fix]users/show でルーム情報を取得するように変更

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  # before_action :authenticate_api_v1_user!
+
   def index
     users = User.all.order(created_at: :desc)
     render json: users
@@ -6,14 +8,34 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     user = User.find_by(id: params[:id])
-    user_list = {
+    user_info = {
       id: user.id,
       name: user.name,
       email: user.email,
       image: user.image,
       address: user.address
     }
-    render json: user_list
+    # 相手とのルーム情報
+    is_room_exist = false
+    my_entries = Entry.where(user_id: current_api_v1_user.id)
+    other_entries = Entry.where(user_id: user.id)
+    my_entries.each do |my_entrie|
+      other_entries.each do |other_entrie|
+        is_room_exist = true if my_entrie.room_id == other_entrie.room_id
+      end
+    end
+    if is_room_exist
+      my_entries.each do |my_entrie|
+        other_entries.each do |other_entrie|
+          if my_entrie.room_id == other_entrie.room_id
+            room = Room.find_by(id: my_entrie.room_id)
+            render json: { user_info:, room_id: room.id }, status: :ok
+          end
+        end
+      end
+    else
+      render json: { user_info: }, status: :ok
+    end
   end
 
   def update
