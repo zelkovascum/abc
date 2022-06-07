9ba320f682a390c67762ad2dc03693696dfb54bd
scum <kyk467575@gmail.com>
2022-06-07 12:51:03 +0900

    [clean]不要なコメント削除

--- api/app/uploaders/avatar_uploader.rb
+++ api/app/uploaders/avatar_uploader.rb
@@ -14,13 +14,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    # if Rails.env.development?
-    #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    # elsif Rails.env.test?
-    #   "uploads/test/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    # else
-    #   "uploads/production/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    # end
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
---
+++ front/reactapp/src/components/atoms/reactions/Reactionkeleton.tsx
@@ -0,0 +1,43 @@
+import React, { FC, memo } from "react";
+import {
+	ListItem,
+	ListItemAvatar,
+	ListItemText,
+	Skeleton,
+	Divider,
+} from "@mui/material";
+
+export const Reactionkeleton: FC = memo(() => {
+	return (
+		<>
+			<Divider />
+			<ListItem sx={{ mx: "auto" }}>
+				<ListItemAvatar>
+					<Skeleton variant="circular" width={40} height={40} />
+				</ListItemAvatar>
+				<ListItemText
+					primary={
+						<Skeleton
+							sx={{
+								width: "100px",
+							}}
+						/>
+					}
+					secondary={
+						<Skeleton
+							sx={{
+								width: {
+									xs: "200px",
+									sm: "300px",
+									md: "400px",
+									lg: "500px",
+									xl: "600px",
+								},
+							}}
+						/>
+					}
+				/>
+			</ListItem>
+		</>
+	);
+});
