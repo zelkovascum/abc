6eb0929ca8bf0362b9ac7aad66d73ba4f3b70aab
scum <kyk467575@gmail.com>
2022-06-10 11:58:50 +0900

    [add]usersテーブルにsnsカラム追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_07_050226) do
+ActiveRecord::Schema.define(version: 2022_06_10_025734) do
 
   create_table "entries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2022_06_07_050226) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "address"
+    t.string "sns"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
