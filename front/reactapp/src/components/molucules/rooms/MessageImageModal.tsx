c8706a0b5bf9deae2aca66fef38265fb0c5802c5
scum <kyk467575@gmail.com>
2022-06-10 22:13:08 +0900

    [add]チャット画像保存機能追加

--- front/reactapp/src/components/molucules/rooms/MessageImageModal.tsx
+++ front/reactapp/src/components/molucules/rooms/MessageImageModal.tsx
@@ -1,6 +1,6 @@
 import { Dispatch, FC, memo, SetStateAction } from "react";
-import { Box, Modal, CardMedia } from "@mui/material";
-import { Modalstyle } from "components/pages/users/Setting";
+import { Box, Modal, CardMedia, Button, Link } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
 
 type Props = {
 	isOpenModal: boolean;
@@ -13,17 +13,35 @@ export const MessageImageModal: FC<Props> = memo((props) => {
 
 	return (
 		<Modal open={isOpenModal} onClose={() => setIsOpenModal(false)}>
-			<Box
-				sx={Modalstyle}
-				// display="flex"
-			>
-				<CardMedia component="img" src={imageUrl} alt="" />
-				<Box>
-					<a href={imageUrl} download={imageUrl}>
-						画像ダウンロード
-					</a>
+			<Box sx={style}>
+				<CardMedia
+					component="img"
+					src={imageUrl}
+					alt=""
+					sx={{ maxHeight: "400px" }}
+				/>
+				<Box textAlign="center">
+					{/* productionで動作 */}
+					<Button sx={{ mt: 1 }}>
+						<Link href={imageUrl} download={imageUrl}>
+							保存
+							<DownloadIcon sx={{ verticalAlign: "-7px" }} />
+						</Link>
+					</Button>
 				</Box>
 			</Box>
 		</Modal>
 	);
 });
+
+const style = {
+	position: "absolute" as "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 300,
+	bgcolor: "background.paper",
+	borderRadius: 2,
+	boxShadow: 24,
+	p: 2,
+};
