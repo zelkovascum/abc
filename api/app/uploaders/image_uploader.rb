f36e66126a7dbffadba804351e560f6264e19468
scum <kyk467575@gmail.com>
2022-06-10 09:51:56 +0900

    [add]mini_magick

--- api/app/uploaders/image_uploader.rb
+++ api/app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.test? || Rails.env.development?
@@ -33,7 +33,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process resize_to_fit: [50, 50]
+  process resize_to_fit: [300, 300]
   # end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
