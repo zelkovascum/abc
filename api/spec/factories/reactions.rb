ac111cd35856cb501b8b5356862c1c0242c8d298
scum <kyk467575@gmail.com>
2022-07-09 17:56:16 +0900

    [add]reactions request テスト追加

--- api/spec/factories/reactions.rb
+++ api/spec/factories/reactions.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :reaction do
     to_user_id { nil }
     from_user_id { nil }
+    matched { false }
   end
 end
--- api/spec/requests/api/v1/reactions_spec.rb
+++ api/spec/requests/api/v1/reactions_spec.rb
@@ -1,10 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Reactions', type: :request do
-  describe 'GET /index' do
-    # pending "add some examples (or delete) #{__FILE__}"
-    it 'test' do
-      expect(1 + 1).to eq 2
+  describe 'post /api/v1/reactions' do
+    context 'normal' do
+      let(:password) { 'password' }
+      let(:user) { create(:user, password:) }
+      let(:other_user) { create(:user) }
+
+      before do
+        @auth_headers = login(user.email, 'password')
+      end
+
+      context 'There is no reaction from the other' do
+        it 'http status code is created' do
+          post(
+            api_v1_reactions_path,
+            headers: @auth_headers,
+            params: { reaction: { from_user_id: user.id, to_user_id: other_user.id } }
+          )
+          expect(response).to have_http_status :created
+        end
+
+        it 'same params and json response in message' do
+          post(
+            api_v1_reactions_path,
+            headers: @auth_headers,
+            params: { reaction: { from_user_id: user.id, to_user_id: other_user.id } }
+          )
+          expect(JSON.parse(response.body)['message']).to eq('リアクションしました')
+        end
+
+        it 'reaction records increases' do
+          expect do
+            post(
+              api_v1_reactions_path,
+              headers: @auth_headers,
+              params: { reaction: { from_user_id: user.id, to_user_id: other_user.id } }
+            )
+          end.to change(Reaction, :count).by 1
+        end
+
+        it 'matched is false' do
+          post(
+            api_v1_reactions_path,
+            headers: @auth_headers,
+            params: { reaction: { from_user_id: user.id, to_user_id: other_user.id } }
+          )
+          expect(Reaction.last.matched).to be false
+        end
+      end
     end
   end
 end
